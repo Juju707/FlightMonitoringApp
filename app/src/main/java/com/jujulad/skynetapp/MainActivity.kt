a190a550256d1da72ac453f4c43cef34ec6f8264
Julia Szymczak <s.jula.zosia@gmail.com>
2020-04-02 14:03:00 +0200

    Added API

--- app/src/main/java/com/jujulad/skynetapp/MainActivity.kt
+++ app/src/main/java/com/jujulad/skynetapp/MainActivity.kt
@@ -2,11 +2,13 @@ package com.jujulad.skynetapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.opensky.model.OpenSkyStates
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //OpenSkyStates state=new OpenSkyStates();
     }
 }
